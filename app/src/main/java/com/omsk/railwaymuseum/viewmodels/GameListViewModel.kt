1ccbf953ac181a8dcb2b051b24593230a0a62727
xiramant <artnic7@yandex.ru>
2021-10-05 10:09:42 +0600

    Addition to "Added game list".

---
+++ app/src/main/java/com/omsk/railwaymuseum/viewmodels/GameListViewModel.kt
@@ -0,0 +1,41 @@
+package com.omsk.railwaymuseum.viewmodels
+
+import androidx.lifecycle.*
+import com.omsk.railwaymuseum.net.game.GameListApi
+import com.omsk.railwaymuseum.net.game.GameListModel
+import kotlinx.coroutines.launch
+
+class GameListViewModel: ViewModel() {
+
+    private val _response = MutableLiveData<List<GameListModel>>()
+
+    val response: LiveData<List<GameListModel>>
+        get() = _response
+
+    init {
+        getGameList()
+    }
+
+    private fun getGameList() {
+        viewModelScope.launch {
+            try {
+                _response.value = GameListApi.retrofitService.getGameListApi()
+            } catch (e: Exception) {
+                _response.value = ArrayList()
+            }
+        }
+    }
+
+
+
+    class Factory : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(GameListViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return GameListViewModel() as T
+            }
+            throw IllegalArgumentException("Unable to construct GameListViewModel")
+        }
+    }
+}
+
